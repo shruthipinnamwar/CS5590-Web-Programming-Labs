2b3439ce2e6bfa33322b625c53cb5981e8833661
Shruthi Pinnamwar <shruthipinnamwar@gmail.com>
2019-03-06 15:55:54 -0600

    adds files to documentations folder fopr lab2 task2

---
+++ Lab 2/Source/Task2/googleKnowledgeSearchApi/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClientJsonpModule, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    HttpClientJsonpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
